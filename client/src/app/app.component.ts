93502315efa584de5a69d06362f771f5c01ad674
Vitaliy Klubkou <vitaliy.klubkou@gmail.com>
2022-02-12 20:23:09 +0300

    Without animation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,9 @@
 import {Component} from '@angular/core';
-import {trigger, transition, useAnimation} from "@angular/animations";
-import {moveFromLeft, rotateCubeToLeft} from "ngx-router-animations";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    animations: [
-        trigger('rotateCubeToLeft', [transition('* => *', useAnimation(rotateCubeToLeft))])
-    ]
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    getState(outlet: any) {
-        return outlet.activatedRouteData.state;
-    }
 }
